eb1f4c8ccbcebfb54fc9f5a4adc71c7983de3814
will <adrianwhp@163.com>
2017-03-10 20:27:17 +0800

    显示屏幕Lable

--- 网易新闻/网易新闻/Classes/Home/Controller/VVHomeController.m
+++ 网易新闻/网易新闻/Classes/Home/Controller/VVHomeController.m
@@ -8,6 +8,7 @@
 
 #import "VVHomeController.h"
 #import "VVChannelModel.h"
+#import "VVChannelLable.h"
 
 @interface VVHomeController ()
 @property (weak, nonatomic) IBOutlet UIScrollView *channelScrollView;
@@ -23,6 +24,9 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
    
+    //  iOS7 提供的,如果有导航栏显示的滚动的视图(UITextView, UITableView, UICollectionView, UIScrollView)内容会自动往下偏移64, 设置no表示不让其偏移
+    self.automaticallyAdjustsScrollViewInsets = NO;
+    
     [self requestChannelData];
     
 }
@@ -32,10 +36,47 @@
     
     NSArray *modelData = [VVChannelModel getChannelModelData];
     
-    for (VVChannelModel *model in modelData) {
+//    for (VVChannelModel *model in modelData) {
+//        
+//        NSLog(@"%@",model);
+//    }
+
+    //遍历频道模型数组,创建对应的频道lable
+    //频道Lable的大小
+    CGFloat lableWidth = 80;
+    CGFloat lableHeight = 44;
+    
+    for (int i = 0; i < modelData.count; i++) {
         
-        NSLog(@"%@",model);
+        //获取对应的模型数据
+        VVChannelModel *model = modelData[i];
+        
+        //创建lable
+        VVChannelLable *channelLable = [[VVChannelLable alloc]initWithFrame:CGRectMake(i * lableWidth, 0, lableWidth, lableHeight)];
+        
+        //获取显示内容
+        channelLable.text = model.tname;
+        
+        //设置文字大小和居中显示
+        channelLable.font = [UIFont systemFontOfSize:15];
+        channelLable.textAlignment = NSTextAlignmentCenter;
+        
+        //添加到视图
+        [self.channelScrollView addSubview:channelLable];
+        
     }
+    
+    //设置scrollView的滚动范围
+    self.channelScrollView.contentSize = CGSizeMake(lableWidth * modelData.count, 0);
+    
+    //取消滚动条
+    self.channelScrollView.showsVerticalScrollIndicator = NO;
+    self.channelScrollView.showsHorizontalScrollIndicator = NO;
+    
+    
+    
+    
+    
     
     
 }
---
+++ 网易新闻/网易新闻/Classes/Home/View/VVChannelLable.m
@@ -0,0 +1,21 @@
+//
+//  VVChannelLable.m
+//  网易新闻
+//
+//  Created by 王惠平 on 2017/3/10.
+//  Copyright © 2017年 Will Wang. All rights reserved.
+//
+
+#import "VVChannelLable.h"
+
+@implementation VVChannelLable
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
